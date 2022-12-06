4e51a7d703ce72747a4ec0e2d26dbd7d5dd44bcd
zthoo <kplichta1998@gmail.com>
2022-12-06 14:26:47 +0100

    added custom error, fixed some bugs, added preloader

--- scripts/index.js
+++ scripts/index.js
@@ -15,33 +15,88 @@ const statsWeight = document.getElementById("statsWeight");
 
 const submitButton = document.getElementById("submitButton");
 
+const Male = document.getElementById("Male");
+const Female = document.getElementById("Female");
+
+const error = document.getElementById("error");
+const errorTitle = document.getElementById("errorTitle");
+const errorDescription = document.getElementById("errorDescription");
+const errorButton = document.getElementById("errorButton");
+
+let Count = 0;
+Male.addEventListener("click", () => {
+  if (Count == 0) {
+    Female.classList.remove("checked");
+    Male.classList.add("checked");
+    Count = 1;
+  }
+  if (Count == 1) {
+    Female.classList.remove("checked");
+    Male.classList.add("checked");
+    Count = 0;
+  }
+});
+
+Female.addEventListener("click", () => {
+  if (Count == 0) {
+    Male.classList.remove("checked");
+    Female.classList.add("checked");
+    Count = 1;
+  }
+  if (Count == 1) {
+    Male.classList.remove("checked");
+    Female.classList.add("checked");
+    Count = 0;
+  }
+});
+
+errorButton.addEventListener("click", () => {
+  error.style.visibility = "hidden";
+  error.style.opacity = "0";
+});
+
 submitButton.addEventListener("click", () => {
   let heightValue = parseInt(heightText.innerHTML);
   let weightValue = parseInt(weightText.innerHTML);
 
-  if (weightValue > 0) {
-    stats.style.visibility = "visible";
-    stats.style.opacity = "1";
-
-    let final = (weightValue / Math.pow(heightValue / 100, 2)).toFixed(1);
-
-    statsFinal.innerHTML = final;
-
-    if (final < 18.5) {
-      statsWeight.style.color = "#E35F5F";
-      statsWeight.innerHTML = "Underweight";
-    } else if (final >= 18.5 && final <= 24.9) {
-      statsWeight.style.color = "#56D463";
-      statsWeight.innerHTML = "Correctly";
-    } else if (final >= 25 && final <= 29.9) {
-      statsWeight.style.color = "#5689D4";
-      statsWeight.innerHTML = "Overweight";
-    } else {
-      statsWeight.style.color = "#D45656";
-      statsWeight.innerHTML = "Obese";
-    }
+  if (Male.classList.contains("checked")) {
+    finalBMI();
+  } else if (Female.classList.contains("checked")) {
+    finalBMI();
   } else {
-    // TODO: zrobić custom error.
-    console.log("Error");
+    error.style.visibility = "visible";
+    error.style.opacity = "1";
+    errorTitle.innerHTML = "Error occured!";
+    errorDescription.innerHTML = "Enter your gender.";
+  }
+
+  function finalBMI() {
+    if (weightValue > 0) {
+      stats.style.visibility = "visible";
+      stats.style.opacity = "1";
+
+      let final = (weightValue / Math.pow(heightValue / 100, 2)).toFixed(1);
+
+      statsFinal.innerHTML = final;
+
+      if (final < 18.5) {
+        statsWeight.style.color = "#E35F5F";
+        statsWeight.innerHTML = "Underweight";
+      } else if (final >= 18.5 && final <= 24.9) {
+        statsWeight.style.color = "#56D463";
+        statsWeight.innerHTML = "Correct";
+      } else if (final >= 25 && final <= 29.9) {
+        statsWeight.style.color = "#5689D4";
+        statsWeight.innerHTML = "Overweight";
+      } else {
+        statsWeight.style.color = "#D45656";
+        statsWeight.innerHTML = "Obese";
+      }
+    } else {
+      error.style.visibility = "visible";
+      error.style.opacity = "1";
+      errorTitle.innerHTML = "Error occured!";
+      errorDescription.innerHTML = "The correct weight value is not given.";
+    }
   }
 });
---
+++ scripts/preloader.js
@@ -0,0 +1,8 @@
+const preloader = document.getElementById("preloader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.style.visibility = "hidden";
+    preloader.style.opacity = "0";
+  }, 1000);
+});
